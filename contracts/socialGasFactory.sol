522f219be7b955b0e547925222fc7f6011791293
Tayo <copyafrica93@gmail.com>
2024-02-27 20:54:49 +0000

    socialGas nft

--- contracts/socialGasFactory.sol
+++ contracts/socialGasFactory.sol
@@ -1,8 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract socialGasFactory{
-    function createSocialGasPost(string memory _name, string memory _symbol)external pure returns (string memory){
-        
+import "./deployedNFTABI.sol";
+
+contract socialGasFactory {
+    deployedNFTABI public deployedContract;
+
+    address public _deployedContractAddress =
+        "0x909532D0979BDF52DA7bB389BBa4Bd95321c9018";
+
+    function createSocialGasPost(
+        string memory _name,
+        string memory _symbol
+    ) external pure returns (string memory) {
+        deployedContract = deployedNFTABI(_deployedContractAddress);
+        deployedContract.constructNewNFT();
     }
 }
